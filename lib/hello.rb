b05cec1223b0a5ca55c26513eb4ceef16471c45c
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-14 03:35:02 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,11 @@
 def hello_t(array)
   i = 0
   while i < array.length
-    #yeild array[i]
+    yeild(array[i])
     #puts array.index(i)
     i += 1
   end
+  array
 end
 
-# call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
 
