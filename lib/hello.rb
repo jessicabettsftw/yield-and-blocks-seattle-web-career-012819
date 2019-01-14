98225822524cb59a09d4132a443ec64ab1bd5d8c
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-14 03:39:02 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,11 @@
 def hello_t(array)
-  i = 0
-  while i < array.length
-    #yeild(array[i])
-    puts array.index(i)
-    i += 1
-  end
+  if block_given?
+    i = 0
+    while i < array.length
+      #yeild(array[i])
+      puts array.index(i)
+      i += 1
+    end
   array
 end
 
